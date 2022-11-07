ea77f0a4679541f1938024809a2bf18a70ba5b12
Juan Pablo <juanpablocamus28@gmail.com>
2022-11-06 21:08:19 -0300

    .

--- api/src/services/userService.js
+++ api/src/services/userService.js
@@ -3,9 +3,14 @@ const bcrypt = require('bcrypt');
 
 const { user } = require('../database');
 
-const registerUser = (req, res) => {
+const registerUser = (req, res, next) => {
   const { email, name, password } = req.body;
 
+  try {
+    //
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
