f759fb9e5d2efcd8779728fdfbaf33aef3ba2674
Juan Pablo <juanpablocamus28@gmail.com>
2022-11-08 09:53:59 -0300

    prettier

--- api/index.js
+++ api/index.js
@@ -20,6 +20,5 @@ const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-  db.sync({ force: true })
-    .then(() => console.log('Database connected'));
+  db.sync({ force: true }).then(() => console.log('Database connected'));
 });
--- api/src/models/Operation.js
+++ api/src/models/Operation.js
@@ -1,17 +1,21 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  sequelize.define('operation', {
-    type: {
-      type: DataTypes.ENUM('Entry', 'Withdraw'),
-      allowNull: false,
+  sequelize.define(
+    'operation',
+    {
+      type: {
+        type: DataTypes.ENUM('Entry', 'Withdraw'),
+        allowNull: false,
+      },
+      amount: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      concept: {
+        type: DataTypes.STRING,
+      },
     },
-    amount: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    concept: {
-      type: DataTypes.STRING,
-    },
-  }, { timestamps: true });
+    { timestamps: true },
+  );
 };
